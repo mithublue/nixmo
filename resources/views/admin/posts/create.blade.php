f0b02e3721f79b08ac313239fb8a85a245f75f48
Mithu A Quayium <cemithu06@gmail.com>
2020-07-12 17:00:56 +0600

    view - Create post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             @include('admin.sidebar')
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Create New Post</div>
-                    <div class="card-body">
-                        <a href="{{ \App\Includes\Classes\Router()->get_route( 'browse', null, 'post_type', 'post' ) }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <br />
-                        <br />
-
-                        @if ($errors->any())
-                            <ul class="alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
-
-                        <form method="POST" action="{{ \App\Includes\Classes\Router()->get_route('store', null, 'post_type', 'post' ) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-                            {{ csrf_field() }}
-                            @include ('admin.posts.form', ['formMode' => 'create'])
-                        </form>
-
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h5>@lang('app.Create New Post')</h5>
                     </div>
                 </div>
+                <a href="{{ \App\Includes\Classes\Router()->get_route( 'browse', null, 'post_type', 'post' ) }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                <br />
+                <br />
+
+                @if ($errors->any())
+                    <ul class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+
+                <form method="POST" action="{{ \App\Includes\Classes\Router()->get_route('store', null, 'post_type', 'post' ) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    @include ('admin.posts.form', ['formMode' => 'create'])
+                </form>
+
             </div>
         </div>
     </div>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-3">
+<div class="col-md-2">
     <div class="card">
         <div class="card-header">
             Sidebar
